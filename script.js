7bdcb8f53caebcbbd906c040122b47d2106eede7
jpierce424 <71786124+jpierce424@users.noreply.github.com>
2021-02-10 08:00:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to', 'program');
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(secretMessage.indexOf('get'), secretMessage.indexOf('time'), 'know');
+console.log(secretMessage.join(' '));
